fdd37664c7188a264e6cd8446be395f620f9b031
xiaxixi <i995170241@163.com>
2019-05-26 22:45:37 +0800

    Create App.vue
    客户端 App.vue

---
+++ train-ticket-client/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Tabbar></Tabbar>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Tabbar from '@/components/Tabbar.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Tabbar
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
