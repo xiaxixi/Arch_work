9087212fba9ddb5eef823af857688d3110f021d6
xiaxixi <i995170241@163.com>
2019-05-28 22:59:06 +0800

    服务端 index.js

---
+++ train-ticket-server/src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors';
+
+import users from './routes/users';
+import fakes from './routes/fakes';
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/users', users);
+app.use('/api/fakes', fakes);
+
+app.get('/*', (req, res) => {
+  res.status(200).json({ msg: 'VUE-BOOKING API' });
+})
+
+app.listen(3000, () => console.log("> Running on localhost:3000"));
