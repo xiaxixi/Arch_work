852428f6b6a8ab5d1cb9cd4e4ce6ae7e67d7690e
xiaxixi <i995170241@163.com>
2019-05-28 23:00:00 +0800

    服务端 userRouter

---
+++ train-ticket-server/src/routes/users.js
@@ -0,0 +1,61 @@
+import models from '../models';
+import express from 'express';
+import Sequelize from 'sequelize';
+import authenticate from '../middlewares/authenticate';
+
+const Op = Sequelize.Op;
+
+var router = express.Router();
+
+router.post('/register', (req, res) => {
+  const { username, password, phone_number, id_no, email, role } = req.body;
+  models.User
+    .create({
+      username, password, phone_number, id_no, email, role
+    })
+    .then(record => {
+      res.status(200).json({
+        code: 1,
+        user: record.toAuthJSON(),
+        msg: 'Register successfully!'
+      });
+    })
+    .catch(err => {
+      res.status(200).json({
+        code: -1,
+        msg: 'Register failed（用户名已存在）!'
+      });
+    });
+});
+
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  models.User
+    .findOne({
+      where: {
+        [Op.or]: [{ username: username }]
+      }
+    })
+    .then(record => {
+      if (record && record.isValidPassword(password)) {
+        res.status(200).json({
+          code: 1,
+          user: record.toAuthJSON(),
+          msg: 'Login successfully!'
+        });
+      } else {
+        res.status(200).json({
+          code: -1,
+          msg: 'Login failed!'
+        });
+      }
+    })
+    .catch(err => {
+      res.status(200).json({
+        code: -1,
+        msg: 'Login failed!'
+      });
+    });
+});
+
+module.exports = router;
