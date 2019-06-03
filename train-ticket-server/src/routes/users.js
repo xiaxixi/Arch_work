8dd68f618e66d8b7dd3c4aeb9a8d32309e029c95
xiaxixi <i995170241@163.com>
2019-06-03 22:15:51 +0800

    mq

--- train-ticket-server/src/routes/users.js
+++ train-ticket-server/src/routes/users.js
@@ -8,18 +8,19 @@ const Op = sequelize.Op;
 
 var router = express.Router();
 
-
-
 //MQ:注册成功时异步发送注册成功邮件，加快系统响应速度
+const nodemailer = require('nodemailer');
 var email_queue = [];
-var email = require('emailjs');
-var server = email.server.connect({
-  user: "1198226333@qq.com",
-  password:"ecewlqeoretuhagg",
-  host:"smtp.qq.com",
-  ssl:true
+let transporter = nodemailer.createTransport({
+  host: 'smtp.qq.com', //发送者邮箱服务端口,参考 https://github.com/nodemailer/nodemailer-wellknown/blob/master/services.json
+  secureConnection: true, // use SSL
+  port: 465,
+  secure: true,
+  auth: {
+    user: '1198226333@qq.com', //发送者邮箱
+    pass: 'ecewlqeoretuhagg'  //发送者邮箱授权码，不是登录或者独立密码，是你设置的smtp授权密码
+  }
 });
-
 
 router.post('/register', (req, res) => {
   const { username, password, phone_number, id_no, email, role } = req.body;
@@ -32,26 +33,30 @@ router.post('/register', (req, res) => {
         code: 1,
         user: record.toAuthJSON(),
         msg: '注册成功！'
-        
-        
-        email_queue.push(email);
-        setTimeout(function(){
-           for(var i = email_queue.length - 1;i >= 0;i--){
-              server.send({
-                text:"Congratulations!\n\nYou've successfully become a member of Train.",
-                from:"1198226333@qq.com",
-                to:email_queue[i],
-                cc:"1198226333@qq.com",
-                subject:"Train"
-              },function(err,message){
-                console.log(err || message);
-              }); 
-              console.log(email_queue[i]);
-              email_queue.pop();
-            }
-          },10);
-  
       });
+
+      email_queue.push(email);
+      setTimeout(function () {
+        for (var i = email_queue.length - 1; i >= 0; i--) {
+          let mailOptions = {
+            from: '1198226333@qq.com', //发送者邮箱
+            to: email_queue[i], //接受者邮箱,可以设置多个
+            subject: 'Train', //邮件主题
+            text: "Congratulations!\n\nYou've successfully become a member of Train."//信箱里邮件列表时，此邮件显示的内容说明
+          };
+
+          transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+              return console.log(error);
+            }
+            console.log(`Message: ${info.messageId}`);
+            console.log(`sent: ${info.response}`);
+          });
+          console.log(email_queue[i]);
+          email_queue.pop();
+        }
+      }, 10);
+
     })
     .catch(err => {
       res.status(200).json({
